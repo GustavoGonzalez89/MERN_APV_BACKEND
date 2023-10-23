5a95a591296f26d7c1233537cdec97d819dc00b8
GustavoGonzalez89 <148630864+GustavoGonzalez89@users.noreply.github.com>
2023-10-23 20:15:24 +0200

    Update index.js
    cambios en cors

--- index.js
+++ index.js
@@ -10,12 +10,35 @@ const app = express();
 app.use(express.json()); //de esa forma le decimos aue vamos a enviarle datos de tipo JSON
 dotenv.config();
 conectarDB();
+// Lista de orígenes aceptados
+const allowedOrigins = [
+    'https://apv-frontend-mu.vercel.app',
+    'http://app-cliente-2.com',
+    // Agrega más orígenes según sea necesario
+];
+// Configurar las opciones de CORS
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Origen no permitido por la política CORS'));
+        }
+    },
+    methods: 'GET,POST,PUT,PATCH,DELETE', // Los métodos HTTP permitidos
+    credentials: true, // Habilita las cookies y encabezados de autorización
+    optionsSuccessStatus: 204 // Responde a las pre-solicitudes OPTIONS con 204
+};
 
-app.use(cors({
-    origin: `https://apv-frontend-beryl.vercel.app`,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-}));
+// Habilitar el middleware CORS con las opciones configuradas
+app.use(cors(corsOptions));
+
+// Middleware para establecer los encabezados personalizados
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', corsOptions.origin);
+    res.header('Access-Control-Allow-Methods', corsOptions.methods);
+    next();
+  });
 
 app.use("/api/veterinarios",veterinarioRoutes);
 app.use("/api/pacientes",pacientesRoutes);
