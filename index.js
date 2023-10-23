6fd6fde8e232155d1eb15fb71b26f9adf0410e6f
GustavoGonzalez89 <148630864+GustavoGonzalez89@users.noreply.github.com>
2023-10-23 22:03:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,35 +10,13 @@ const app = express();
 app.use(express.json()); //de esa forma le decimos aue vamos a enviarle datos de tipo JSON
 dotenv.config();
 conectarDB();
-// Lista de orígenes aceptados
-const allowedOrigins = [
-    'https://apv-frontend-mu.vercel.app',
-    'http://app-cliente-2.com',
-    // Agrega más orígenes según sea necesario
-];
-// Configurar las opciones de CORS
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Origen no permitido por la política CORS'));
-        }
-    },
-    methods: 'GET,POST,PUT,PATCH,DELETE', // Los métodos HTTP permitidos
-    credentials: true, // Habilita las cookies y encabezados de autorización
-    optionsSuccessStatus: 204 // Responde a las pre-solicitudes OPTIONS con 204
-};
 
-// Habilitar el middleware CORS con las opciones configuradas
-app.use(cors(corsOptions));
-
-// Middleware para establecer los encabezados personalizados
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', corsOptions.origin);
-    res.header('Access-Control-Allow-Methods', corsOptions.methods);
-    next();
-  });
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST'); // Especificamos que solo permitimos solicitudes POST
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use("/api/veterinarios",veterinarioRoutes);
 app.use("/api/pacientes",pacientesRoutes);
