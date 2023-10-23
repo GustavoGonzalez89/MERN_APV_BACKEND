809082201ca0aef1629cdf936028a8b6bb89b6b2
GustavoGonzalez89 <148630864+GustavoGonzalez89@users.noreply.github.com>
2023-10-23 23:17:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,36 @@ app.use(express.json()); //de esa forma le decimos aue vamos a enviarle datos de
 dotenv.config();
 conectarDB();
 
-// Configuración CORS para toda la aplicación
-app.use(
-  cors({
-    origin: 'https://apv-frontend-mu.vercel.app', // Reemplaza con el dominio permitido
-    methods: 'GET,POST,PUT,PATCH,DELETE', // Métodos HTTP permitidos
+// Lista de orígenes aceptados
+const allowedOrigins = [
+    'https://apv-frontend-mu.vercel.app',
+    'http://app-cliente-2.com',
+    // Agrega más orígenes según sea necesario
+];
+// Configurar las opciones de CORS
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Origen no permitido por la política CORS'));
+        }
+    },
+    methods: 'GET,POST,PUT,PATCH,DELETE', // Los métodos HTTP permitidos
     credentials: true, // Habilita las cookies y encabezados de autorización
-    optionsSuccessStatus: 204, // Responde a las pre-solicitudes OPTIONS con 204
-    allowedHeaders: 'Content-Type, Authorization', // Encabezados permitidos
-  })
-);
+    optionsSuccessStatus: 204 // Responde a las pre-solicitudes OPTIONS con 204
+};
+
+// Habilitar el middleware CORS con las opciones configuradas
+app.use(cors(corsOptions));
+
+// Middleware para establecer los encabezados personalizados
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', corsOptions.origin);
+    res.header('Access-Control-Allow-Methods', corsOptions.methods);
+    next();
+  });
+
 
 app.use("/api/veterinarios",veterinarioRoutes);
 app.use("/api/pacientes",pacientesRoutes);
